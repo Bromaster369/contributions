34c749d44416451fa35685bec94f1c7f3c5b0eb0
Bromaster369 <chrispardopersonal@gmail.com>
2021-08-14 20:20:40 -0400

    created instance method for creating html regarding the data for each object. Create instance method to take element and append it to the contributions container

--- contributions_client/src/contribution.js
+++ contributions_client/src/contribution.js
@@ -4,6 +4,7 @@
 class Contribution {
     //this will remember objects
     static all = []
+    static contributionContainer = document.getElementById("contributions-container")
     // we define a class variable by using the word static 
      
     //this constructor will mimic the backend 
@@ -27,5 +28,25 @@ class Contribution {
         //this is referecing the newly made instance
     }
  
+    contributionHTML(){
+        //this is an instance method, it's goint ot be called on the contribution instance
+        //this function is just reponsible for creating the html regarding the data for each object is going to go in 
+        //this method is going to be responsible for creating the inner html for this "element" 
+        this.element.innerHTML += `
+        <div>
+            <h3>${this.name}</h3>
+            <h2>${this.email}</h2>
+            <p>${this.description}</p>
+        </div>
+        `
+        // we want a return for our function 
+        return this.element
+    }
 
+    imposeOnDom(){
+        // takes element and appends it to our contributions container 
+        Contribution.contributionContainer.append(contributionHTML())
+    }
+
+    //these two functions have been separated to manipulate and update the data of the item we just edited without having to reappend it to the DOM because it might cause duplication 
 }
